2f9bb23829aedbf610af20ae6641abdecfe845af
minh <minh@playstudios.asia>
2026-01-25 22:04:57 +0700

    - Forget this class

---
+++ DataModel/GeneralConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE2FlipTool.DataModel
+{
+    public class GeneralConfig
+    {
+        public string googleSheetID { get; set; }
+        public string googleSheetName { get; set; }
+        
+
+        public GeneralConfig(string googleSheetID, string googleSheetName)
+        {
+            this.googleSheetID = googleSheetID;
+            this.googleSheetName = googleSheetName;
+        }
+    }
+}
