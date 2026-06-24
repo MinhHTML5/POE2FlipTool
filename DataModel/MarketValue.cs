e273342e462c8f2c547d5df059f0c09bce433a30
Imperius <lorendoor@gmail.com>
2026-06-25 04:32:11 +0700

    wip

--- DataModel/MarketValue.cs
+++ DataModel/MarketValue.cs
@@ -1,13 +1,13 @@
 ﻿namespace POE2FlipTool.DataModel
 {
-    public record MarketRecord(float Rate, float Volume);
+    public record MarketListing(float Rate, float Volume);
 
     public class MarketValue
     {
         public string ItemBuyName { get; set; } = "";
         public string ItemSellName { get; set; } = "";
-        public List<MarketRecord> AvailableRate { get; set; } = new();
-        public List<MarketRecord> CompetingRate { get; set; } = new();
+        public List<MarketListing> AvailableRate { get; set; } = new();
+        public List<MarketListing> CompetingRate { get; set; } = new();
     }
 
 }
